c386e7f559ed5bac856caba899b335d8c05fd568
towficaziz <tapolly01@gmail.com>
2024-04-28 15:14:14 +0200

    adding project gallery

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,36 @@ import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
 const Card = ({ title, description, imageSrc }) => {
-  return null;
+  return (
+    <VStack
+      maxW="sm"
+      // borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="lg"
+      color="#5e5e5e"
+      background="white"
+      
+    >
+      <Image src={imageSrc} alt={title} borderRadius="lg"/>
+
+      <VStack p={4} align="start">
+        <Heading as="h3" size="md" color="black">
+          {title}
+        </Heading> 
+        <Text fontSize="xs">{description}</Text>
+      </VStack>
+
+      <HStack p={4} alignItems="baseline" justify="flex-start" w="full">
+        <a href="#">
+          <Text fontWeight="bold" display="inline" marginRight="2">
+            See more <FontAwesomeIcon icon={faArrowRight} size="1x" />
+          </Text>
+        </a>
+      </HStack>
+
+    </VStack>
+  );
 };
 
 export default Card;
